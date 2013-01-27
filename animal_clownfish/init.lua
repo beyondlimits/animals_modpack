f5ac86e3fe6f44faf3e85051527cfa87ff7d76aa
sapier <Sapier at GMX dot net>
2013-01-27 01:55:59 +0000

    add clownfish mesh model

--- animal_clownfish/init.lua
+++ animal_clownfish/init.lua
@@ -1,15 +1,27 @@
-local version = "0.0.8"
+-------------------------------------------------------------------------------
+-- Mob Framework Mod by Sapier
+-- 
+-- You may copy, use, modify or do nearly anything except removing this
+-- copyright notice. 
+-- And of course you are NOT allow to pretend you have written it.
+--
+--! @file init.lua
+--! @brief clownfish implementation
+--! @copyright Sapier
+--! @author Sapier
+--! @date 2013-01-27
+--
+-- Contact sapier a t gmx net
+-------------------------------------------------------------------------------
+minetest.log("action","MOD: animal_clownfish mod loading ...")
+
+local version = "0.0.9"
 
 local selectionbox_clownfish = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5}
 
 local clownfish_groups = {
 						not_in_creative_inventory=1
 					}
-
-local modpath = minetest.get_modpath("animal_clownfish")
-
---include debug trace functions
-dofile (modpath .. "/model.lua")
 
 function clownfish_drop()
 	local result = {}
@@ -32,47 +44,69 @@ clownfish_prototype = {
 					},
 					groups = clownfish_groups,
 					envid = "open_waters"
-				},				
-		movement =  {
+				},
+		movement = {
 					default_gen="probab_mov_gen",
 					min_accel=0.2,
 					max_accel=0.3,
 					max_speed=1.5,
 					pattern="swim_pattern2",
 					canfly=true,
-					},		
-		harvest        = nil,
+					},
 		catching = {
 					tool="animalmaterials:net",
 					consumed=true,
-					},				  	
-		random_drop    = nil,		
-		auto_transform = nil,					
-		graphics = {
-					sprite_scale={x=1,y=1},
-					sprite_div = {x=6,y=1},
-					visible_height = 1,
-					visible_width = 1,
 					},
-      graphics_3d = {
-            visual = "wielditem",
-            textures = {"animal_clownfish:box_clownfish"},
-            collisionbox = selectionbox_clownfish,
-            visual_size= {x=0.666,y=0.666,z=0.666},
-            },
-		combat         = nil,
-		
-		spawning = {		
+		spawning = {
 					rate=0.02,
 					density=350,
 					algorithm="in_shallow_water_spawner",
 					height=-1,
 					respawndelay = 60,
 					},
+		animation = {
+				swim = {
+					start_frame = 81,
+					end_frame   = 155,
+					},
+				stand = {
+					start_frame = 1,
+					end_frame   = 80,
+					},
+				},
+		states = {
+				{ 
+					name = "default",
+					movgen = "none",
+					chance = 0,
+					animation = "stand",
+					graphics_3d = {
+						visual = "mesh",
+						mesh = "animal_clownfish.b3d",
+						textures = {"animal_clownfish_mesh.png"},
+						collisionbox = selectionbox_clownfish,
+						visual_size= {x=1,y=1,z=1},
+						},
+					graphics = {
+						sprite_scale={x=1,y=1},
+						sprite_div = {x=6,y=1},
+						visible_height = 1,
+						visible_width = 1,
+					},
+					typical_state_time = 30,
+				},
+				{ 
+					name = "swiming",
+					movgen = "probab_mov_gen",
+					chance = 0.9,
+					animation = "swim",
+					typical_state_time = 180,
+				},
+				},
 		}
 
 
 --register with animals mod
-print ("Adding animal "..clownfish_prototype.name)
-animals_add_animal(clownfish_prototype)
-print ("animal_clownfish mod version " .. version .. " loaded")
+minetest.log("action","\tadding animal "..clownfish_prototype.name)
+mobf_add_mob(clownfish_prototype)
+minetest.log("action","MOD: animal_clownfish mod       version " .. version .. " loaded")
