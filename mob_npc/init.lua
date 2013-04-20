631c55d8e30b9fa763439faa9728124d6e54ac56
sapier <Sapier at GMX dot net>
2013-04-21 01:23:59 +0200

    update trader inventory

--- mob_npc/init.lua
+++ mob_npc/init.lua
@@ -15,7 +15,7 @@
 -------------------------------------------------------------------------------
 minetest.log("action","MOD: mob_npc mod loading ...")
 
-local version = "0.0.12"
+local version = "0.0.13"
 local npc_groups = {
 						not_in_creative_inventory=1
 					}
@@ -173,9 +173,9 @@ npc_trader_prototype = {
 		},
 		trader_inventory = {
 				goods = {
-							{ "default:mese 1", "default:dirt 99", "default:cobble 50"},
-							{ "default:steel_ingot 1", "default:mese_crystal 5", "default:cobble 20"},
-							{ "default:stone 5", "default:mese_crystal 1", "default:cobble 50"},
+							{ "default:mese 1", "default:dirt 99", "default:cobble 60"},
+							{ "doors:door_wood 1", "default:mese_crystal 1", "default:cobble 10"},
+							{ "default:fence_wood 20", "default:mese_crystal 5", "default:cobble 25"},
 							{ "animalmaterials:saddle 1", "default:mese 1", "default:cobble 50"},
 							{ "default:sword_steel 1", "default:mese_crystal 4", "default:stone 20"},
 							{ "bucket:bucket_empty 1", "default:cobble 10", "default:stone 2"},
@@ -183,11 +183,12 @@ npc_trader_prototype = {
 							{ "default:shovel_steel 1", "default:mese_crystal 2", "default:stone 10"},
 							{ "default:axe_steel 1", "default:mese_crystal 2", "default:stone 22"},
 							{ "default:torch 33", "default:mese_crystal 2", "default:stone 10"},
-							{ "default:ladder 12", "default:mese_crystal 1", "default:stone 5"},
+							{ "default:ladder 12", "default:mese 1", "default:cobble 50"},
 							{ "default:paper 12", "default:mese_crystal 2", "default:stone 10"},
-							{ "default:chest 1", "default:mese_crystal 2", "default:stone 10"},
+							{ "default:chest_locked 1", "default:mese_crystal 5", "default:cobble 25"},
 							{ "mob_archer:archer 1","default:mese_crystal 10",nil},
 							{ "mob_guard:guard 1","default:mese_crystal 10",nil},
+							{ "doors:door_steel 1","default:mese_crystal 3","default:cobble 20"},
 						},
 				random_names = { "Hans","Franz","Xaver","Fritz","Thomas","Martin"},
 			}
