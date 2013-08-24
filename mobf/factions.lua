1f6839c91858f615161acc5d07788b639293706b
sapier <Sapier at GMX dot net>
2013-08-24 11:21:22 +0200

    Fix rightclick factions menu beeing absolutely useless

--- mobf/factions.lua
+++ mobf/factions.lua
@@ -41,40 +41,85 @@ end
 --! @brief creat factions a mob may be member of
 --! @memberof mobf_factions
 --
---! @param factions data of mob
+--! @param factions_data of mob
 --
 -------------------------------------------------------------------------------
 function mobf_factions.setupmob(factionsdata)
-	if mobf_rtd.factions_available and factionsdata ~= nil and
-		factionsdata.member ~= nil then
-		
-		for i=1,#factionsdata.member,1 do
-			if not factions.exists(factionsdata.member[i]) then
-				factions.add_faction(factionsdata.member[i])
+	if mobf_rtd.factions_available then
+		if factionsdata ~= nil and
+			factionsdata.member ~= nil then
+			
+			for i=1,#factionsdata.member,1 do
+				if not factions.exists(factionsdata.member[i]) then
+					factions.add_faction(factionsdata.member[i])
+				end
 			end
 		end
 	end
 end
 
 -------------------------------------------------------------------------------
--- name: setupentity(entity)
+-- name: setupentity(entity,preserved_data)
 --
 --! @brief add a mob to it's factons
 --! @memberof mobf_factions
 --
 --! @param entity to be added to it's factions
+--! @param preserved_data mob reputation data
 --
 -------------------------------------------------------------------------------
-function mobf_factions.setupentity(entity)
-	if mobf_rtd.factions_available and entity.data.factions ~= nil and
-		entity.data.factions.member ~= nil then
+function mobf_factions.setupentity(entity,preserved_data)
+	if mobf_rtd.factions_available then
+		if entity.data.factions ~= nil and
+			entity.data.factions.member ~= nil then
+			
+			for i=1,#entity.data.factions.member,1 do
+				factions.member_add(entity.data.factions.member[i],entity)
+			end
+		end
 		
-		for i=1,#entity.data.factions.member,1 do
-			factions.member_add(entity.data.factions.member[i],entity)
+				
+		if preserved_data ~= nil then
+			for k,v in pairs(preserved_data) do
+				reputation = factions.get_reputation(k,entity.object)
+				
+				if reputation ~= 0 and reputation ~= v then
+					local delta = v - reputation
+					
+					factions.modify_reputation(k,entity.object,delta)
+				else
+					factions.modify_reputation(k,entity.object,v)
+				end
+			end
 		end
 	end
 end
 
+-------------------------------------------------------------------------------
+-- name: cleanup_entity(entity)
+--
+--! @brief called on deactivation of a mob
+--! @memberof mobf_factions
+--
+--! @param entity to be cleant up
+--! @param preserved_data mob reputation data
+--
+-------------------------------------------------------------------------------
+function mobf_factions.cleanupentity(entity)
+	local retval = {}
+	
+	local factionlist = factions.get_faction_list()
+	
+	for i=1,#factionlist,1 do
+		reputation = factions.get_reputation(factionlist[i],entity.object)
+		
+		if reputation ~= 0 then
+			retval[factionlist[i]] = reputation
+		end
+	end
+	
+	return retval
+end
 
 -------------------------------------------------------------------------------
 -- name: mob_rightclick_callback(entity,player)
@@ -166,26 +211,47 @@ end
 --! @return true/false show menu again
 -------------------------------------------------------------------------------
 function mobf_factions.handle_rightclick(current_fields,fields,formname,player,fieldname)
+	print("current_fields: " .. dump(current_fields))
 	--try to read data from global store
 	local menu_data = mobf_global_data_get(current_fields[3])
 	if menu_data ~= nil then
-		if current_fields[2]  == "btn_set_reputation" then
-			local factionlist = factions.get_faction_list()
-			if fields["te_reputation"] ~= nil and
-				menu_data.selected ~= nil and
-				menu_data.selected > 0 and
-				menu_data.selected <= #factionlist then
-					reputation = factions.get_reputation(factionlist[menu_data.selected],menu_data.entity.object)
+		if current_fields[2]  == "btn_add_mob_faction" then
+			if menu_data.tl_owner_selected ~= nil then
+				local factionlist_owner = factions.get_factions(menu_data.player)
+				
+				if factionlist_owner ~= nil and
+					#factionlist_owner >= menu_data.tl_owner_selected then
 					
-					local delta = fields["te_reputation"] - reputation
-					
-					factions.modify_reputation(factionlist[menu_data.selected],menu_data.entity.object,delta)
+					local toadd = factionlist_owner[menu_data.tl_owner_selected]
+					factions.member_add(toadd,menu_data.entity.object)
+				end
 			end
-		elseif current_fields[2] == "tl_selected_faction" then
+		elseif current_fields[2]  == "btn_drop_mob_faction" then
+			if menu_data.tl_mob_selected ~= nil then
+				local factionlist_mob = factions.get_factions(menu_data.entity.object)
+				
+				local todel = factionlist_mob[menu_data.tl_mob_selected]
+				
+				if factions.is_member(todel,player) then
+					factions.member_remove(todel,menu_data.entity.object)
+				else
+					--TODO maybe send chat message
+					print("MOBF Factions you're not member of " .. todel .. " so you can't remove a mob from it")
+				end
+			else
+				print("MOBF Factions: trying to delete from faction but no faction selected")
+			end
+		elseif current_fields[2] == "tl_mob_factions" then
 			local event = explode_textlist_event(fields[fieldname])
 			
 			if event.typ ~= "INV" then
-				menu_data.selected = event.index
+				menu_data.tl_mob_selected = event.index
+			end
+		elseif current_fields[2] == "tl_owner_factions" then
+			local event = explode_textlist_event(fields[fieldname])
+			
+			if event.typ ~= "INV" then
+				menu_data.tl_owner_selected = event.index
 			end
 		end
 	else
@@ -217,33 +283,52 @@ function mobf_factions.show_mob_factions_menu(new_id,menu_data)
 	
 	--check if menu creator is owner of the specific mob
 	if menu_data.entity.dynamic_data.spawning.spawner == playername then
-		formspec = formspec .. "size[4,7.5]"
-				.."label[0,-0.25;Factions:]"
-				.."textlist[0,0.3;3.7,6;mobf_factions:tl_selected_faction:" .. new_id .. ";"
-				
-		local factionlist = factions.get_faction_list()
 	
-		if #factionlist ~= 0 then
-			for i=1,#factionlist,1 do
-				formspec = formspec .. factionlist[i] .. ","
-			end
-		else
-			formspec = formspec .. "no factions available"
+		if menu_data.tl_mob_selected == nil then
+			menu_data.tl_mob_selected = 0
 		end
 		
-		local reputation = ""
-		if menu_data.selected == nil then
-			menu_data.selected = 0
+		if menu_data.tl_owner_selected == nil then
+			menu_data.tl_owner_selected = 0
 		end
-		if	menu_data.selected > 0 and
-			menu_data.selected <= #factionlist then
-			reputation = factions.get_reputation(factionlist[menu_data.selected],menu_data.entity.object)
-		end
+	
+		formspec = formspec .. "size[8,8]"
+				.."label[0,-0.25;Member of factions:]"
+				.."textlist[0,0.3;3.7,7;mobf_factions:tl_mob_factions:" .. new_id .. ";"
 				
-		formspec = formspec .. ";" ..menu_data.selected .. ";false]"
-				.."label[0,6.4;Reputation:]"
-				.."field[2,6.75;2.2,0.5;te_reputation;;" .. reputation .. "]"
-				.."button[0,7.25;3.9,0.5;mobf_factions:btn_set_reputation:" .. new_id .. ";Set reputation]"
+		local factionlist_mob = factions.get_factions(menu_data.entity.object)
+	
+		if #factionlist_mob ~= 0 then
+			for i=1,#factionlist_mob,1 do
+				formspec = formspec .. factionlist_mob[i] .. ","
+			end
+		else
+			formspec = formspec .. "not member of any faction"
+		end
+		
+		formspec = formspec .. ";" .. menu_data.tl_mob_selected .. "]"
+		
+		local factionlist_player = factions.get_factions(menu_data.player)
+		
+		formspec = formspec
+				.."label[4,-0.25;Owner factions:]"
+				.."textlist[4,0.3;3.7,7;mobf_factions:tl_owner_factions: " .. new_id .. ";"
+	
+		if #factionlist_player ~= 0 then
+			for i=1,#factionlist_player,1 do
+				formspec = formspec .. factionlist_player[i] .. ","
+			end
+		else
+			formspec = formspec .. "not member of any faction"
+		end
+		
+		formspec = formspec .. ";" .. menu_data.tl_owner_selected .. "]"
+		
+		formspec = formspec
+			.. "button[0,7.5;3.95,0.5;mobf_factions:btn_drop_mob_faction:" .. new_id .. ";Remove from faction]"
+			.. "button[4,7.5;3.95,0.5;mobf_factions:btn_add_mob_faction:" .. new_id .. ";<-- Mob join faction]"
+			
+		
 	else
 		formspec = "label[0,0;This is not your mob keep away!]"
 	end
