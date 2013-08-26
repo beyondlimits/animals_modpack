33b4c7f888835f31db6e1d989b8e505b935e3250
sapier <Sapier at GMX dot net>
2013-08-26 23:54:28 +0200

    Improve lifebar offset calculation for large mobs

--- mobf/lifebar.lua
+++ mobf/lifebar.lua
@@ -72,7 +72,12 @@ end
 function mobf_lifebar.add(entity)
 	local pos = entity.object:getpos()
 	local BS = 10
-	pos.y = pos.y + entity.collisionbox[5] + 0.1
+	
+	local lifebar_offset = (MAX(entity.collisionbox[4]-entity.collisionbox[1],
+								entity.collisionbox[6]-entity.collisionbox[3]) / 0.5) * 0.4
+	lifebar_offset = lifebar_offset * lifebar_offset
+	
+	pos.y = pos.y + entity.collisionbox[5] + lifebar_offset
 
 	local lifebar = minetest.add_entity(pos,"mobf:lifebar")
 	
