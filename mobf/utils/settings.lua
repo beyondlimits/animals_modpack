bf50194161b77f774e78c3beaae6959508f9e53e
sapier <Sapier at GMX dot net>
2013-11-23 13:57:58 +0100

    Improve settings format to be more human readable

--- mobf/utils/settings.lua
+++ mobf/utils/settings.lua
@@ -1,8 +1,8 @@
 -------------------------------------------------------------------------------
 -- Mob Framework Mod by Sapier
--- 
+--
 -- You may copy, use, modify or do nearly anything except removing this
--- copyright notice. 
+-- copyright notice.
 -- And of course you are NOT allow to pretend you have written it.
 --
 --! @file settings.lua
@@ -30,39 +30,94 @@ function mobf_init_world_specific_settings()
 
 	if minetest.world_setting_set == nil or
 		type(minetest.world_setting_set) ~= "function" then
-		
+
 		local mobf_world_path = minetest.get_worldpath()
-	
+
 		--initialy read settings file
 		mobf_world_settings_data = nil
 		local file,error = io.open(mobf_world_path .. "/mobf_settings.conf","r")
-		
+
 		if error ~= nil then
 			minetest.log(LOGLEVEL_WARNING,"MOBF: failed to open world specific config file")
 			mobf_world_settings_data = {}
 		else
-			mobf_world_settings_data = minetest.deserialize(file:read("*a"))
+			local data_raw = file:read("*a")
+
+			data_raw=data_raw:gsub("\n","")
+			mobf_world_settings_data = minetest.deserialize(data_raw)
 			file:close()
 		end
-		
+
 		--set world settings function
 		minetest.world_setting_set = function(name,value)
 			mobf_world_settings_data[name] = value
-		
+
 			local file,error = io.open(mobf_world_path .. "/mobf_settings.conf","w")
-			
+
 			if error ~= nil then
 				minetest.log(LOGLEVEL_ERROR,"MOBF: failed to open world specific config file")
 			end
 			mobf_assert_backtrace(file ~= nil)
-			
-			file:write(minetest.serialize(mobf_world_settings_data))
+
+			local serialized_data = minetest.serialize(mobf_world_settings_data)
+
+			local prefix=""
+			local lastwasnewline = false
+			local commahandled = false
+			local got_non_space_char = false
+
+			for i = 1, #serialized_data do
+				local c = serialized_data:sub(i,i)
+
+				if (c == "{") then
+					if not lastwasnewline then
+						file:write("\n")
+					end
+					file:write(prefix .. c .. "\n")
+					prefix = prefix .. "    "
+					lastwasnewline = true
+					got_non_space_char = false
+				elseif (c == "}") then
+					if not lastwasnewline then
+						file:write("\n")
+					end
+					prefix = prefix:sub(1,#prefix-4)
+					local nextchar = serialized_data:sub(i+1,i+1)
+					if nextchar == "," then
+						file:write(prefix .. c ..nextchar .. "\n")
+						commahandled = true
+					else
+						file:write(prefix .. c .. "\n")
+					end
+					got_non_space_char = false
+					lastwasnewline = true
+				elseif (c == ",") then
+					if not commahandled then
+						file:write(c .. "\n")
+					end
+					commahandled = false
+					lastwasnewline = true
+					got_non_space_char = false
+				elseif (c == " ") then
+					if got_non_space_char then
+						file:write(c)
+					end
+				else
+					if lastwasnewline then
+						file:write(prefix)
+					end
+					file:write(c)
+					lastwasnewline = false
+					got_non_space_char = true
+				end
+			end
+			file:write("\n")
 			file:close()
 		end
-		
+
 		minetest.world_setting_get = function(name)
 			mobf_assert_backtrace(mobf_world_settings_data ~= nil)
-			
+
 			return mobf_world_settings_data[name]
 		end
 	end
