218c44e529e0e227522df6b447b07a92a596c614
sapier <Sapier at GMX dot net>
2013-11-25 18:42:15 +0100

    Add workaround for broken core line of sight

--- mobf/utils/generic_functions.lua
+++ mobf/utils/generic_functions.lua
@@ -357,6 +357,36 @@ end
 --! @return: true/false
 -------------------------------------------------------------------------------
 function mobf_line_of_sight(pos1,pos2)
+
+	--print("Checking line of sight between "..printpos(pos1).." and "..printpos(pos2))
+	local distance = mobf_calc_distance(pos1,pos2)
+
+	local normalized_vector = {	x=(pos2.x-pos1.x)/distance,
+					y=(pos2.y-pos1.y)/distance,
+					z=(pos2.z-pos1.z)/distance}
+
+
+	local line_of_sight = true
+
+	for i=1,distance, 1 do
+		local tocheck = { x=pos1.x + (normalized_vector.x * i),
+					y=pos1.y + (normalized_vector.y *i),
+					z=pos1.z + (normalized_vector.z *i)}
+
+		local node = minetest.env:get_node(tocheck)
+
+
+		if minetest.registered_nodes[node.name] == nil or
+			minetest.registered_nodes[node.name].sunlight_propagates ~= true then
+			line_of_sight = false
+			break
+		end
+	end
+
+	return line_of_sight
+end
+
+function mobf_line_of_sightX(pos1,pos2)
 	return minetest.line_of_sight(pos1,pos2)
 end
 
