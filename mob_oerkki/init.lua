6ac9c1527d0429b44923cdb7e7063059b9924d94
Sapier <sapier AT gmx dot net>
2015-12-25 01:37:24 +0100

    Oerkki: make oerkki vulnerable by sword

--- mob_oerkki/init.lua
+++ mob_oerkki/init.lua
@@ -14,7 +14,7 @@
 -- Contact sapier a t gmx net
 -------------------------------------------------------------------------------
 minetest.log("action","MOD: mob_oerkki mod loading ...")
-local version = "0.2.0"
+local version = "0.2.1"
 
 local oerkki_groups = {
 						not_in_creative_inventory=1
@@ -38,7 +38,7 @@ local oerkki_prototype = {
 					base_health=3,
 					kill_result="animalmaterials:meat_toxic 1",
 					armor_groups= {
-						fleshy=15,
+						fleshy=30,
 						deamon=30,
 					},
 					groups = oerkki_groups,
